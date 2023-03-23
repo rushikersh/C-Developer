8099526ec995ff2db0725037b84ed8f0326e8ac2
dark_pokimon <93254171+rushikersh@users.noreply.github.com>
2023-03-23 16:19:27 +0530

    Update savepid.cpp

--- savepid.cpp
+++ savepid.cpp
@@ -11,16 +11,16 @@ int savePidFile()
          */
         if (access(PIDFILE, F_OK) == 0)
                 if (unlink(PIDFILE) < 0)
-			{
-				cout<<"Unlink Failed\n";
-				return -1;
-			}
+                        {
+                                cout<<"Unlink Failed\n";
+                                return -1;
+                        }
         FILE *pf = fopen(PIDFILE, "a+");
         if (pf == NULL)
-	{
-		cout<<"File Descriptor Fail\n";
+        {
+                cout<<"File Descriptor Fail\n";
                 return -1;
-	}
+        }
         /**
          * Process ID will be saved in file. This is required to check if another instance is already running of same binary. Some tools like initctl requires PID file when it starts the daemon.
          */
@@ -28,16 +28,26 @@ int savePidFile()
         fclose(pf);
         return 0;
 }
+void removePidFile()
+{
+                /**
+                 *          * It is required to remove PID file before terminating the process. In case of failed to remove PID file, ignored.
+                 *                   */
+                unlink(PIDFILE);
+}
 
 int main()
 {
-	if(savePidFile()==0)
-	{
-		char b[1024]="" ;
-		sprintf(b,"cat %s",PIDFILE);
-		system(b);
-	}
-	else 
-		cout<<"Not save\n";
-	cout<<endl;
+        if(savePidFile()==0)
+        {
+                char b[1024]="" ;
+                sprintf(b,"cat %s",PIDFILE);
+                system(b);
+        }
+        else
+                cout<<"Not save\n";
+        cout<<endl;
+
+        sleep(2);
+        removePidFile();
 }
